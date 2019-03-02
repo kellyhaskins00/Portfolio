2107dc27dc59f7ffe7c13b65d79b0eac294fbe3f
Kelly Haskins <kellyhaskins00@gmail.com>
2019-03-02 11:11:15 -0800

    form updated

--- contact.php
+++ contact.php
@@ -1,5 +1,17 @@
 <?php
-if($_POST["message"]) {
-    mail("kellyhaskins00@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="kellyhaskins00@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
+
 ?>
